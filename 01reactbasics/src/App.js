bb75a028739e695feb906aa86a89a0b7b3590edc
Aman Shahi <amanshahidev@gmail.com>
2026-01-25 10:45:57 +0530

    completed lesson 1

---
+++ 01reactbasics/src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <h1>Chai aur React | Aman Shahi</h1>
+      <p>
+        In this lecture we learn about:
+        <ul>
+          <li>How to read documentation</li>
+          <li>How to create react projects using create-react-app and vite</li>
+          <li>What is the folder structure of a react project</li>
+          <li>What are the addons with react (like for web, it is react-dom and for mobile apps, it is react-native)</li>
+          <li>What are the scripts in create-react-app and vite</li>
+          <li>What are the different dependencies we get in create-react-app and vi</li>
+        </ul>
+      </p>
+    </>
+  );
+}
+
+export default App;
